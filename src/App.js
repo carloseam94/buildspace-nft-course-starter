1a665759016cbd8af6f4c723c84fc80f808db25a
Carlos <carloseam94@gmail.com>
2021-12-17 19:38:01 -0500

    before adding spinner

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const TWITTER_HANDLE = 'carloseam94';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x1ae531cbC64B337e6018214F715D5F1F105DDCa3";
 
 const App = () => {
 
@@ -44,7 +45,11 @@ const App = () => {
     if (accounts.length !== 0) {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
-      setCurrentAccount(account)
+      setCurrentAccount(account);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener()
     } else {
       console.log("No authorized account found");
     }
@@ -61,6 +66,15 @@ const App = () => {
           alert("Get MetaMask!");
           return;
         }
+
+        let chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+
+        // String, hex code of the chainId of the Rinkebey test network
+        const rinkebyChainId = "0x4"; 
+        if (chainId !== rinkebyChainId) {
+          alert("You are not connected to the Rinkeby Test Network!");
+        }
   
         /*
         * Fancy method to request access to account.
@@ -72,13 +86,46 @@ const App = () => {
         */
         console.log("Connected", accounts[0]);
         setCurrentAccount(accounts[0]); 
+
+        // Setup listener! This is for the case where a user comes to our site
+        // and connected their wallet for the first time.
+        setupEventListener() 
       } catch (error) {
         console.log(error)
       }
     }
 
+    // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
     const askContractToMintNft = async () => {
-      const CONTRACT_ADDRESS = "0xCa1649d4AaE178d03fF1EE1279Ce1cB80B5d9778";
     
       try {
         const { ethereum } = window;
@@ -122,9 +169,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Smash Facts NFTs</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+           100% True Facts, no clickbait
           </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
@@ -134,6 +181,9 @@ const App = () => {
             </button>
           )}
         </div>
+        <div>
+          <a class="button gradient-text" target="_blank" rel="noreferrer" href="https://testnets.opensea.io/collection/squarenft-i9b36zjvvo">See Collection</a>
+        </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
@@ -141,7 +191,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
